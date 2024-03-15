1ac9aae7df94a380cb0b58a1450992b62dd83f9c
arjunrana6 <arjunranaaaofficial@gmail.com>
2024-03-16 04:18:54 +0530

    7 commit

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const http = require("http");
 
 const cors = require("cors");
 const app = express()
-// https://intouchlive.netlify.app
-// http://localhost:3000
 app.use(cors({
     origin: 'https://kouchlive.netlify.app',
     credentials: true,
@@ -26,10 +24,10 @@ let male = [{
     summary: "I'm a wildlife photographer, and I'm obsessed with capturing amazing pictures of animals in their natural homes. My life always remain on the go. I love the thrill of getting that perfect shot. When I'm not chasing wildlife, you'll find me exploring new places, meeting interesting people, and soaking in the wonders of nature. If animal kingdom excites you, then I am your guide."
 },
 {
-    name: 'Matheus',
+    name: 'Hiten',
     age: '21',
     gender: 'male',
-    location: 'Canada',
+    location: 'India',
     pid: '41Z3069',
     summary: 'Music lover 🎵 | Wanderlust soul ✈️ | Bookworm 📚 | Coffee addict ☕ | Creative thinker 🎨 | Fitness freak 💪 | Avid traveler 🌏. Planning my life, to retire in early 30s. Haha'
 },
@@ -48,84 +46,81 @@ let male = [{
     location: 'India',
     pid: '60B3350',
     summary: "Influencer marketing head 9 to 5. Couch potato & reels scroller 5 to 11. I have worked with major indian influencers and if you are interested in some gossip or insight, then I can make your day.Jk, bound under NDA.Miss those travelling days as an intern and auto wale bhaiya ki exciting stories.Looking for someone with different perpective and domain."
+},
+{
+    name: 'Dustin',
+    age: '25',
+    gender: 'male',
+    location: 'USA',
+    pid: '60C3350',
+    summary: "I am gay, telling you in advance because people usually leave the chat after knowing this and I don't want to hide it. If you are open to talking to a gay guy, then you are most welcome. Beside that I work in finance sector, loves to play baseball. And currently, 21 Pilots are my stress buster. See ya."
 }]
-
-// let female = [
-//     {
-//         name: 'Yuvika',
-//         age: '19',
-//         gender: 'female',
-//         location: 'India',
-//         pid: '69P2976',
-//         summary: " A dedicated foodie, pet lover, and part-time author. From exploring street food to cuddling up with my furry friends, life is too kind to me. And in my free time, I plan someone's murder, not to afraid, I write crime thriller novels. Hehe "
-//     },
-//     {
-//         name: 'Katrin',
-//         age: '23',
-//         gender: 'female',
-//         location: 'Brazil',
-//         pid: '68E3255',
-//         summary: 'Eat, Job, Club, Sex, Eat, Sleep and Repeat. Hoping to get a break over here. Gosh, connect with me if you are not in a rat race.'
-//     },
-//     {
-//         name: 'Falak',
-//         age: '18',
-//         gender: 'female',
-//         location: 'India',
-//         pid: '60V3348',
-//         summary: "I listen to Desi Hip Hop music (Strange I know). Will be starting my college life soon. Fingers crossed, hope it doesn't end in vain like school life. Loves to stare at stars at night and a big time procrastinator. Hip Hop lovers are invited."
-//     },
-//     {
-//         name: 'Airah',
-//         age: '20',
-//         gender: 'female',
-//         location: 'India',
-//         pid: '60G3350',
-//         summary: "I live in my own world of delulu dreams and fantasy almost full time. I got lot of stories to share and ready to consume also. No time for BF-GF, already committed, just need a good friend to understand me.If you also need one, then I could be the best option for you.If things went right, we might turn it into a long term friendship and might do meet upin future. Hoping for someone good."
-//     },
-//     {
-//         name: 'Ritika',
-//         age: '17',
-//         gender: 'female',
-//         location: 'India',
-//         pid: '60V3350',
-//         summary: "I am a bathroom singer, and gaming enthu. I have played a wide range of games from mobile to console. Assassin's Creed & Riders Republic are my fav. Would love match with gaming buddy and non gaming buddies are welcome to. But keep in mind besides games I might not have much things to talk."
-//     },
-//     {
-//         name: 'Olivia',
-//         age: '26',
-//         gender: 'female',
-//         location: 'USA',
-//         pid: '68Y3255',
-//         summary: " I live in texas, work full time but I usually have free time in the day to talk. Besides for going to work I’m usually a homebody. Like, 99% of the time I’m either at work, home, or helping my single mother. Just got broke up, so no commitments rn. I love watching movies and tv shows, especially action, comedy, thriller, some horror. Love to connect"
-//     },
-//     {
-//         name: 'Eshita',
-//         age: '22',
-//         gender: 'female',
-//         location: 'India',
-//         pid: '68X3255',
-//         summary: "Today was my first day at office and it went horribly bad. I was fucking crying from inside, only I know how I controlled my tears. I am fucking terrified for my next day. And all those people looking me with their wide eyes, is more embarrassing. Looking for someone to release my stress. If anyone went through this experience is much needed."
-//     },
-//     {
-//         name: 'Valentina',
-//         age: '30',
-//         gender: 'female',
-//         location: 'Mexico',
-//         pid: '69Y3255',
-//         summary: "I struggle with depression and chronic pain. I had to give up my profissional life because of my illness and then realised I don't really have any friends. I always had a great relationship with people, but ever since I got sick they have turned their back on me. Looking for positivity and positive friends. High hopes"
-//     },
-// ]
 
 let female = [
     {
-        name: 'Yuvika',
-        age: '20',
+        name: 'Chestha',
+        age: '19',
         gender: 'female',
         location: 'India',
         pid: '69P2976',
-        summary: " A dedicated foodie, pet lover, and part-time author. From exploring street food to cuddling up with my furry friends, life is too kind to me. And in my free time, I plan someone's murder, not to afraid, I write crime thriller novels. Hehe "
-    }
+        summary: "A night owl. Sleeps in day and work at night. Dark circles are my only friends. I am open to add new friends to my group"
+    },
+    {
+        name: 'Katrin',
+        age: '23',
+        gender: 'female',
+        location: 'Brazil',
+        pid: '68E3255',
+        summary: 'Eat, Job, Club, Sex, Eat, Sleep and Repeat. Hoping to get a break over here. Gosh, connect with me if you are not in a rat race.'
+    },
+    {
+        name: 'Falak',
+        age: '18',
+        gender: 'female',
+        location: 'India',
+        pid: '60V3348',
+        summary: "I listen to Desi Hip Hop music (Strange I know). Will be starting my college life soon. Fingers crossed, hope it doesn't end in vain like school life. Loves to stare at stars at night and a big time procrastinator. Hip Hop lovers are invited."
+    },
+    {
+        name: 'Airah',
+        age: '20',
+        gender: 'female',
+        location: 'India',
+        pid: '60G3350',
+        summary: "I live in my own world of delulu dreams and fantasy almost full time. I got lot of stories to share and ready to consume also. No time for BF-GF, already committed, just need a good friend to understand me.If you also need one, then I could be the best option for you.If things went right, we might turn it into a long term friendship and might do meet upin future. Hoping for someone good."
+    },
+    {
+        name: 'Ritika',
+        age: '17',
+        gender: 'female',
+        location: 'India',
+        pid: '60V3350',
+        summary: "I am a bathroom singer, and gaming enthu. I have played a wide range of games from mobile to console. Assassin's Creed & Riders Republic are my fav. Would love match with gaming buddy and non gaming buddies are welcome to. But keep in mind besides games I might not have much things to talk."
+    },
+    {
+        name: 'Olivia',
+        age: '26',
+        gender: 'female',
+        location: 'USA',
+        pid: '68Y3255',
+        summary: " I live in texas, work full time but I usually have free time in the day to talk. Besides for going to work I’m usually a homebody. Like, 99% of the time I’m either at work, home, or helping my single mother. Just got broke up, so no commitments rn. I love watching movies and tv shows, especially action, comedy, thriller, some horror. Love to connect"
+    },
+    {
+        name: 'Abhilasha',
+        age: '22',
+        gender: 'female',
+        location: 'India',
+        pid: '68X3255',
+        summary: "Today was my first day at office and it went horribly bad. I was fucking crying from inside, only I know how I controlled my tears. I am fucking terrified for my next day. And all those people looking me with their wide eyes, is more embarrassing. Looking for someone to release my stress. If anyone went through this experience is much needed."
+    },
+    {
+        name: 'Valentina',
+        age: '30',
+        gender: 'female',
+        location: 'Mexico',
+        pid: '69Y3255',
+        summary: "I struggle with depression and chronic pain. I had to give up my profissional life because of my illness and then realised I don't really have any friends. I always had a great relationship with people, but ever since I got sick they have turned their back on me. Looking for positivity and positive friends. High hopes"
+    },
 ]
 
 var id = 30;
@@ -283,11 +278,13 @@ app.post("/skipUser", async (req, res) => {
                 }
             }
         }
-
+        var status = '';
+        if (receiver_id.pid === '68X3255' || receiver_id.pid === '60G3350' || receiver_id.pid === '60G3350' || receiver_id.pid === '68Y3255' || receiver_id.pid === '41Z3069' || receiver_id.pid === '20T3162' || receiver_id.pid === '35O2883') status = 'decline'
         res.status(200).json({
             success: true,
             receiver_id,
-            obj: user_map.get(req.body.userId)
+            obj: user_map.get(req.body.userId),
+            status
         })
     }
 
